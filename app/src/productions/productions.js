86939e36f94dfb4c5261dcb1bdab0003fe412617
mcwebdev <mc@frontendtrends.com>
2015-12-22 12:36:31 -0800

    update

--- app/src/productions/productions.js
+++
@@ -1,8 +0,0 @@
-(function(){
-  'use strict';
-
-  // Prepare the 'shows' module for subsequent registration of controllers and delegates
-  angular.module('productions', [ 'ngMaterial' ]);
-
-
-})();
